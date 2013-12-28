bb510e9fe65544031f4e1c1329759f1ddd74ae8d
caizhijiang <54394636@qq.com>
2013-12-28 22:58:25 +0800

    urlparse

--- src/tttt.py
+++ src/tttt.py
@@ -6,9 +6,14 @@ Created on 2013-12-22 下午08:18:32
 @author: CaiKnife
 '''
 import urlparse
+import urllib
 
 url = 'http://example.com/index.php?a=1&b=2&c=3'
 
 r = urlparse.urlparse(url)
 d = urlparse.parse_qs(r.query)
 print d.keys()
+
+d = urlparse.parse_qsl(r.query)
+print d
+print urllib.urlencode(d)
