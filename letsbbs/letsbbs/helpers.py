7faa3b2e452105ff8818f2c7c3b0642da3b2fa13
caizhijiang <54394636@qq.com>
2013-12-28 22:30:58 +0800

    加上了以前写的Python代码

--- letsbbs/letsbbs/helpers.py
+++
@@ -1,6 +0,0 @@
-def register_blueprint(app):
-    from .views import account, frontend
-
-    app.register_blueprint(account, url_prefix='/account')
-    # put this in the end 
-    app.register_blueprint(frontend, url_prefix='')
--- letsbbs/letsbbs/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from account import User, Post
-from ._base import db
-
-__all__ = [
-    'User', 'Post', 
-    'db'
-]
