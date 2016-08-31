1efd541601afacdb6c2802fe8079d53b214e4876
CaiKnife <caiknife@foxmail.com>
2016-08-31 10:45:29 +0800

    format code

--- src/LearnPythonTheHardWay/ex47/tests/NAME_tests.py
+++ src/LearnPythonTheHardWay/ex47/tests/NAME_tests.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python
-#coding: UTF-8
-'''
+# coding: UTF-8
+"""
 Created on 2012-12-15
 
 @author: caiknife
-'''
+"""
 
 from nose.tools import *
 from ex47.game import Room
 
+
 def test_room():
-    gold = Room("GoldRoom", 
+    gold = Room("GoldRoom",
                 """This room has gold in it you can grab. There's a
                 door to the north.""")
     assert_equal(gold.name, "GoldRoom")
     assert_equal(gold.paths, {})
-    
+
+
 def test_room_paths():
     center = Room("Center", "Test room in the center.")
     north = Room("North", "Test room in the north.")
@@ -24,6 +26,7 @@ def test_room_paths():
     center.add_paths({'north': north, 'south': south})
     assert_equal(center.go('north'), north)
     assert_equal(center.go('south'), south)
+
 
 def test_map():
     start = Room("Start", "You can go west and down a hole.")
