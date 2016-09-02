19f43ae99b1314986b29c0bea903e2745e8c79c8
CaiKnife <caiknife@foxmail.com>
2016-09-02 12:29:38 +0800

    requests demo

--- src/requests_demo/demo.py
+++ src/requests_demo/demo.py
@@ -7,3 +7,8 @@ r = requests.get("http://www.163.com")
 
 print type(r.content)
 print type(r.text)
+
+r = requests.post('http://httpbin.org/post', data={'key': 'value'})
+
+print r.url
+print r.encoding
