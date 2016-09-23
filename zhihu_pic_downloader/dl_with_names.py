a8ff78f90e1e7286b04fd083031c6844742294d7
CaiKnife <caiknife@foxmail.com>
2016-09-23 11:04:24 +0800

    use unicode strings

--- zhihu_pic_downloader/dl_with_names.py
+++ zhihu_pic_downloader/dl_with_names.py
@@ -24,14 +24,14 @@ def get_url():
     args = sys.argv
     # 如果不是dl.py zhihu_url这种格式的话，抛出异常
     if len(args) != 2:
-        raise Exception("Wrong number for args, please use Zhihu question url!")
+        raise Exception(u"Wrong number for args, please use Zhihu question url!")
 
     zhihu_url = args[1]
     # zhihu_url不符合问题页面url格式的话，抛出异常
-    re_exp = re.compile(r"^https://www\.zhihu\.com/question/(\d+)")
+    re_exp = re.compile(ur"^https://www\.zhihu\.com/question/(\d+)")
     match = re_exp.match(zhihu_url)
     if not match:
-        raise Exception("Zhihu url is invalid!")
+        raise Exception(u"Zhihu url is invalid!")
 
     entity['url'] = zhihu_url
     entity['question'] = match.groups()[0]
@@ -51,7 +51,7 @@ def load_page():
 
     resp = requests.get(entity['url'], headers=header)
     if resp.status_code != 200:
-        raise Exception("Http error!")
+        raise Exception(u"Http error!")
 
     d = pq(resp.content)
     title = d('title').text()
