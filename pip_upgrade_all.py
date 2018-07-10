a29488da3ba217d10744ee35dc6058c46f796cc8
caiknife <caiknife@foxmail.com>
2018-07-10 18:10:50 +0800

    done

--- pip_upgrade_all.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding:utf8
-
-import pip
-from subprocess import call
-
-for dist in pip.get_installed_distributions():
-    call("pip install -U " + dist.project_name, shell=True)
